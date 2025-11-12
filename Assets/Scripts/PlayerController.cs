8b26eed2102be5138339d34d81428f0b3b265a11
Karam Matar <karammtr@gmail.com>
2025-11-12 14:51:13 +0000

    added player movement, and ability to shoot pizza with space

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 17.0f;
+
+    public GameObject projectilePrefab;
     void Start()
     {
         
@@ -13,6 +18,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+        }
+
+
+
     }
 }
