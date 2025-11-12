bf79f09a0f968477ec5b8c4d0262d6cdb43757c5
Karam Matar <karammtr@gmail.com>
2025-11-12 15:20:09 +0000

    applied DestroyOutOfBound to animals

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float destroyZRange = 30.0f;
+    public float destroyZRangeUP = 30.0f;
+    public float destroyZRangeDown = -20.0f;
     void Start()
     {
         
@@ -14,7 +15,12 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > destroyZRange)
+        if (transform.position.z > destroyZRangeUP)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < destroyZRangeDown)
         {
             Destroy(gameObject);
         }
